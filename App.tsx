a0797eb2a190be0f0f9345c2818796354be10333
KaraElBatson <210316035@ogr.cbu.edu.tr>
2025-11-01 23:28:37 +0300

    Ensures proper initialization of gesture handler
    Moves the import statement for 'react-native-gesture-handler' to the top of the files.
    
    This ensures that the gesture handler is initialized correctly, preventing potential issues with gesture-based components.

--- App.tsx
+++ App.tsx
@@ -1,8 +1,8 @@
+import 'react-native-gesture-handler';
 import React from 'react';
 import { StatusBar } from 'expo-status-bar';
 import { SafeAreaProvider } from 'react-native-safe-area-context';
 import RootNavigator from './src/navigation/RootNavigator';
-import 'react-native-gesture-handler';
 
 export default function App() {
   return (
